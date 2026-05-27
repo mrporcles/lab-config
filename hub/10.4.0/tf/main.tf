b1dce17e945c3528c104aa3430ff6053957feedb
Paul Wiggett <mrporcles@users.noreply.github.com>
2026-05-27 09:17:36 +0100

    Update main.tf

--- hub/10.4.0/tf/main.tf
+++ hub/10.4.0/tf/main.tf
@@ -6,14 +6,3 @@ provider "google" {
 locals {
   zone_suffix = "cf-app.com"
 }
-
-resource "google_compute_route" "nat-vm-route" {
-  name                   = "${var.env_name}-nat-vm-route"
-  dest_range             = "0.0.0.0/0"
-  network                = "${var.env_name}-pcf-network"
-  next_hop_instance      = "${var.env_name}-nat-vm"
-  next_hop_instance_zone = "${var.zone}"
-  description            = "Route to external network via next-hop VM"
-  priority               = 100
-  tags                   = ["system", "postgres", "kafka", "clickhouse-metrics", "prometheus", "control", "blobstore", "registry", "compute", "database", "router", "controller", "errands"]
-}

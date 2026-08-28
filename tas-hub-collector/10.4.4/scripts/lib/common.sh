c304c2a32f1133dfc02fcc7342289acf92fe0124
Paul Wiggett <paul.wiggett@broadcom.com>
2026-08-28 18:09:20 +0100

    reduce retry timeout to 60s

--- tas-hub-collector/10.4.4/scripts/lib/common.sh
+++ tas-hub-collector/10.4.4/scripts/lib/common.sh
@@ -18,7 +18,7 @@ retry() {
     - "$@" && return
     status=$?
     echo "command failed, retrying in 5 mins" 1>&2
-    sleep 300
+    sleep 60
     (( tries-- ))
   done
   echo "error exit: $status" 1>&2
